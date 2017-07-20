bec5844d425365008e5b2fffcacbe9bcb0ab0f82
Elijah Bitting <elijahjbitting@gmail.com>
2017-07-20 12:35:15 -0300

    hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,24 @@
+import sys
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+        @classmethod
+        def setUpClass(cls):
+            for arg in sys.argv:
+                if 'liveserver' in arg:
+                    cls.server_url = 'http://' + arg.split('=')[1]
+                    return
+            super().setUpClass()
+            cls.server_url = cls.live_server_url
+
+        @classmethod
+        def tearDownClass(cls):
+            if cls.server_url == cls.live_server_url:
+                super().tearDownClass()
+        
         def setUp(self):
                 self.browser = webdriver.Firefox()
                 self.browser.implicitly_wait(3)
@@ -22,7 +37,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         
         def test_layout_and_styling(self):
 		# Edith goes to the homepage
-                self.browser.get(self.live_server_url)
+                self.browser.get(self.server_url)
                 self.browser.set_window_size(1024, 768)
                 # she notices that the input box is nicely centered 
                 inputbox = self.browser.find_element_by_id('id_new_item')
@@ -51,7 +66,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         def test_can_start_a_test_and_retrieve_it_later(self):
                 # Edith has heard about a cool new online to-do app. She goes
                 # to check out it's homepage
-                self.browser.get(self.live_server_url)
+                self.browser.get(self.server_url)
 
                 # She notices that the page title and header mention to-do lists
                 self.assertIn('To-Do', self.browser.title)
@@ -107,7 +122,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
                 
                 # Francis visits the home page. There is no sign of Edith's 
                 # list
-                self.browser.get(self.live_server_url)
+                self.browser.get(self.server_url)
                 page_text = self.browser.find_element_by_tag_name('body').text
                 self.assertNotIn(newItemText1, page_text)
                 self.assertNotIn('make a fly', page_text)
